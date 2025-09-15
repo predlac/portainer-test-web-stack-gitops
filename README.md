47dc3490229c7593482abefab400706f81d3f23c
Phil Calder <4473109+predlac@users.noreply.github.com>
2025-09-15 12:51:43 +1200

    Initial commit of the web stack

---
+++ README.md
@@ -0,0 +1,12 @@
+# Test Stack
+
+Create a test stack in Portainer using GitOps
+
+## The Stack
+
+This repo contians the necessary files for the stack
+
+- [docker-compose.yml](docker-compose.yml)
+- [nginx.conf](nginx.conf)
+- [app.py](app.py)
+
---
+++ app.py
@@ -0,0 +1,16 @@
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+r = redis.Redis(host="redis")
+
+@app.route("/")
+def hello():
+    try:
+        visits = r.incr("counter")
+    except:
+        visits = "Cannot connect to Redis."
+    return f"Hello World! This page has been viewed {visits} times."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
